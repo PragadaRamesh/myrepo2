284e3b8dd9c5e0349022813c9bbe4ba9506ca6b8
PragadaRamesh <rameshpragada6@gmail.com>
2025-11-06 14:48:39 +0530

    Update app.java

--- app.java
+++ app.java
@@ -28,3 +28,10 @@ public class HelloWorld {
     }
 }
 
+\\devoleper 2 code is added
+
+    public class Main {
+   public static void main(String[] args) {
+       System.out.println("Hello World");
+   }
+}
